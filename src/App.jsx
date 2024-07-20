dbc384aca46eb22c19d37bd2ea0fa268458fb272
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2024-07-20 22:29:22 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ function App() {
   return (
     <>
     <div className='train'>
-      <div>
+      <div className='train-box'>
       <Move/>        
       </div>
-      <div>
-
+      <div className='content-box'>
+        <h1 className='content-1'>Welcome To</h1>
+        <h3 className='content-2'>Go Through</h3>
       </div>
     </div>
     <Navbar/>
