a70e93d675c5b54a330e80e60215ee9f82d9a20f
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2024-07-21 18:43:41 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import About from './Components/About';
 
 function App() {
   return (
+    <>
     <div className="absolute top-0 z-[-2] h-auto w-full rotate-360 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(209,250,253,1.8)_100%)]">
-      <Navbar/>
       <Hero/>
       <About/>
     </div>
+    <Navbar/>
+    </>
   );
 }
 
