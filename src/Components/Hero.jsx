d7bb80335eba1709e1081ce5755ab3e1538ecf82
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2024-07-21 13:44:48 +0530

    n

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,12 +13,12 @@ const Move = () => {
   return (
   <>
   <div className="train relative top-20 m-2 p-2 flex justify-between items-center">
-    <div className="home-text text-7xl text-wrap font-bold">
-        <p className="para fixed left-10 top-[15rem] ">
+    <div className="home-text text-6xl text-wrap font-bold">
+        <p className="para fixed w-100% left-[5rem] top-[17rem] ">
           Great Design <br /> come alive with <br />motion
         </p>
     </div>
-    <div className="animation-train fixed right-0 w-[55rem] top-[4rem]">
+    <div className="animation-train fixed -right-11 w-[55rem] top-[4rem]">
       {View}
     </div>
   </div>
