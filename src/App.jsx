5bf77c612e48fd82bb73ed87e44dc09bcf281c06
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2024-07-21 11:47:39 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from './Components/Hero';
 function App() {
   return (
     <>
+    <div class="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(241,245,255,1.8)_100%)]"></div>
     <Hero/>
     <Navbar/>
     </>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -12,16 +12,13 @@ const Move = () => {
 
   return (
   <>
-  <div className="train relative top-20 m-2 p-2 flex gap-4 justify-between items-center">
-    <div className="home-text text-8xl mt-2 ml-10 text-wrap m-2 p-2 font-bold">
-        <p>
+  <div className="train relative top-20 m-2 p-2 flex justify-between items-center">
+    <div className="home-text text-7xl text-wrap font-bold">
+        <p className="para fixed left-10 top-[15rem] ">
           Great Design <br /> come alive with <br />motion
         </p>
-        {/* <button className="getstated bg-blue-300 p-2 text-4xl m-4 font-medium rounded-lg border-2 border-black hover:border-4 hover:border-blue-200">
-          Get Started
-        </button> */}
     </div>
-    <div className="animation-train w-1/2">
+    <div className="animation-train fixed right-0 w-[55rem] top-[4rem]">
       {View}
     </div>
   </div>
