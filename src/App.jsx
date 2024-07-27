fe57272c6bf74f3dfe3c131ee136f05bd5b3b1fe
Navjot Singh <navjotsingh16082003@gmail.com>
2024-07-27 14:23:58 +0530

    wasted time

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import About from './Components/About';
-import Selection from './Components/Selection';
-import Contact from './Components/Contact';
-import Footer from './Components/Footer';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './Contexts/authContext';
+import Header from './Components/Common/Header';
+import Home from './Pages/Home';
+import Login from './Components/Auth/Login';
+import Register from './Components/Auth/Register';
 
-
-function App() {
-  return (
-    <>
-    <Navbar/>
-    <div className="absolute top-0 z-[-2] h-auto w-full rotate-360 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(209,250,253,1.8)_100%)]">
-    <Hero/>
-    <About/>
-    <Selection/>
-    <Contact/>
-    <Footer/>
-    </div>
-    </>
-  );
-}
+const App = () => {
+    return (
+        <AuthProvider>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
+};
 
 export default App;
---
+++ src/Components/Common/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <nav className="bg-gray-800 text-white p-4">
+            <ul className="flex space-x-4">
+                <li><Link to="/" className="hover:underline">Home</Link></li>
+                <li><Link to="/about" className="hover:underline">About</Link></li>
+                <li><Link to="/contact" className="hover:underline">Contact</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
