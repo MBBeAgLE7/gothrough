f98d6c852ef686709117594aed8f5b4897058f16
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2024-07-27 15:18:49 +0530

    n

---
+++ gothrough-main/gothrough-main/src/Components/Hero.jsx
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+import { useLottie } from "lottie-react";
+import groovyWalkAnimation from "./groovyWalk.json";
+import { Link } from "react-scroll";
+
+const Hero = () => {
+  const options = {
+    animationData: groovyWalkAnimation,
+    loop: true,
+  };
+
+  const { View } = useLottie(options);
+
+
+
+  return (
+    <>
+      <div className="hero train m-2 p-2 flex justify-between mt-8">
+        <div className="home-text text-6xl ml-36 mt-36 text-wrap font-bold">
+          <p className="para w-100%">
+            Great Design <br /> come alive with <br />motion
+          </p>
+          <button className="font-bold text-xl bg-blue-600 text-white p-3 rounded-xl mt-8">
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-80}
+              duration={800}
+            >
+              Get Started
+            </Link>
+          </button>
+        </div>
+        <div className="animation-train ml-32 pt-20 w-[55rem] h-[55rem]">
+          {View}
+        </div>
+      </div>
+    </>
+  )
+};
+
+export default Hero;
