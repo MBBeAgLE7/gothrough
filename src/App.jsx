ecd2ccb8d441ffb66025b32fb0dc6bd7046bb99d
MBBeAgLE7 <120193425+MBBeAgLE7@users.noreply.github.com>
2024-07-27 19:25:56 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -7,30 +7,40 @@ import Selection from './Components/Selection';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 import axios from 'axios';
+import Card from './Components/Card';
 
 function App() {
   const [contain, setContain] = useState([]);
+  const [train, setTrain] = useState('');
+
+  const onChangeTrain = (e) => {
+    setTrain(e.target.value);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch(`https://real-time-pnr-status-api-for-indian-railways.p.rapidapi.com/name/${8119853741}`, {
+      if (train.length === 5) {
+        const url = 'https://trainjourney-irctc-api.p.rapidapi.com/api/train/trainv2?Disclaimer=This%20train%20running%20information%20is%20not%20affiliated%20with%20or%20endorsed%20by%20Indian%20Railways%20or%20IRCTC.&date=07032024&apikey=5eb5f408&search=12814';
+        const options = {
           method: 'GET',
           headers: {
             'x-rapidapi-key': '03a6fc1c64msh9ca81b434e634bfp123553jsnd3933bc0fec7',
-            'x-rapidapi-host': 'real-time-pnr-status-api-for-indian-railways.p.rapidapi.com'
+            'x-rapidapi-host': 'trainjourney-irctc-api.p.rapidapi.com'
           }
-        });
-        const data = await response.json();
-        setContain(data);
-        console.log(data);
-      } catch (err) {
-        console.error(err);
+        };
+
+        try {
+          const response = await fetch(url, options);
+          const result = await response.text();
+          console.log(result);
+        } catch (error) {
+          console.error(error);
+        }
       }
     };
 
     fetchData();
-  }, []);
+  }, [train]);
 
   return (
     <>
@@ -39,6 +49,13 @@ function App() {
         <Hero />
         <About />
         <Selection />
+        <Card />
+        <input
+          className="m-10 p-3"
+          placeholder="Enter The Name Here"
+          onChange={onChangeTrain}
+          value={train}
+        ></input>
         <Contact />
         <Footer />
       </div>
--- src/Components/Selection.jsx
+++ src/Components/Selection.jsx
@@ -1,24 +1,10 @@
 import React, { useState } from 'react';
 
 const Selection = () => {
-  const [stnName, setstnName] = useState('');
-
-  const onchangestnName = (e) => {
-    setstnName(e.target.value);
-  };
 
   return (
     <>
-      <div className="selection-card bg-yellow-400">
-        hello world
-      </div>
-      <div className='m-10'>
-        <input 
-          placeholder='Enter the station name' 
-          className='bg-white relative pl-1 p-10' 
-          onChange={onchangestnName}
-        />
-      </div>
+      
     </>
   );
 }
